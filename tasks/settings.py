47f72b79ddaf4f3c1eb410aaf89bffff98286744
nurhesen <33550425+nurhesen@users.noreply.github.com>
2021-11-30 19:55:03 +0400

    Update settings.py

--- tasks/settings.py
+++ tasks/settings.py
@@ -79,12 +79,20 @@ WSGI_APPLICATION = 'tasks.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'postgres',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'pgdb',
+        'PORT': 5432,
+        'DISABLE_SERVER_SIDE_CURSORS': True,
+
     }
 }
+
 
 
 # Password validation
